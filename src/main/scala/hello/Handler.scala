12a2bc7cd8dbb75ea5bdb3fd77e9beb48483a157
Nahid Hayati <nahid.hayati@gmail.com>
2023-07-02 21:44:38 -0700

    resolve compile error

--- src/main/scala/hello/Handler.scala
+++ src/main/scala/hello/Handler.scala
@@ -1,16 +1,12 @@
 package hello
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
-import org.apache.logging.log4j.{LogManager, Logger}
 
 import scala.jdk.CollectionConverters._
 
 class Handler extends RequestHandler[Request, Response] {
 
-  val logger: Logger = LogManager.getLogger(getClass)
-
   def handleRequest(input: Request, context: Context): Response = {
-    logger.info(s"Received a request: $input")
     Response("Go Serverless v1.0! Your function executed successfully!", input)
   }
 }
